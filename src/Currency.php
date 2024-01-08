e14306ceb7f227991ac292a1acedcb0989e018ad
Umid Kurbanov <49520470+umidkurbanov@users.noreply.github.com>
2024-01-08 20:40:57 +0500

    Update Currency.php

--- src/Currency.php
+++ src/Currency.php
@@ -9,11 +9,24 @@ class Currency
     private static function getJson()
     {
         $response = Http::get('https://nbu.uz/exchange-rates/json/');
-        return $response->body();
+        return json_decode($response->body());
     }
 
     public static function convert($from, $to)
     {
-        return self::getJson();
+        $from_price = 1;
+        $to_price = 1;
+
+        foreach (self::getJson() as $item) {
+            $code = strtolower($item->code);
+
+            if ($code == $from)
+                $from_price = $item->cb_price;
+
+            if ($code == $to)
+                $to_price = $item->cb_price;
+        }
+
+        return $from_price / $to_price;
     }
 }
