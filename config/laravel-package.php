6e3d53483c76657210946a72cc0f86798ac118ca
Umid Kurbanov <49520470+umidkurbanov@users.noreply.github.com>
2024-01-08 18:33:22 +0500

    1

--- config/laravel-package.php
+++ config/laravel-package.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'url' => env('LARAVEL_PACKAGE_URL', 'http://localhost'),
+    'username' => env('LARAVEL_PACKAGE_URL', 'http://localhost'),
     'secret' => env('LARAVEL_PACKAGE_SECRET'),
 ];
