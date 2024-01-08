88d13e8ed9e284fea0a966df90665809da742384
Umid Kurbanov <49520470+umidkurbanov@users.noreply.github.com>
2024-01-08 20:48:46 +0500

    Update Currency.php

--- src/Currency.php
+++ src/Currency.php
@@ -14,19 +14,34 @@ class Currency
 
     public static function convert(string $from, string $to, float $amount)
     {
-        $from_price = 1;
-        $to_price = 1;
+        $from_price = 0;
+        $from_price_nbu_buy = 0;
+        $from_price_nbu_sell = 0;
+
+        $to_price = 0;
+        $to_price_nbu_buy = 0;
+        $to_price_nbu_sell = 0;
 
         foreach (self::getJson() as $item) {
             $code = strtolower($item->code);
 
-            if ($code == $from)
+            if ($code == $from) {
                 $from_price = $item->cb_price;
+                $from_price_nbu_buy = $item->nbu_buy_price;
+                $from_price_nbu_sell = $item->nbu_cell_price;
+            }
 
-            if ($code == $to)
+            if ($code == $to) {
                 $to_price = $item->cb_price;
+                $to_price_nbu_buy = $item->nbu_buy_price;
+                $to_price_nbu_sell = $item->nbu_cell_price;
+            }
         }
 
-        return number_format(($from_price * $amount) / $to_price, 2, '.', ' ');
+        return [
+            'cb_price' => number_format(($from_price * $amount) / $to_price, 2, '.', ' '),
+            'nbu_buy_price' => number_format(($from_price_nbu_buy * $amount) / $to_price_nbu_buy, 2, '.', ' '),
+            'nbu_cell_price' => number_format(($from_price_nbu_sell * $amount) / $to_price_nbu_sell, 2, '.', ' '),
+        ];
     }
 }
